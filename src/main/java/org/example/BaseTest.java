37a740d821d4bbb73bb4615cd3ca8f5773ecb03d
Nikita Shults <hukutqaaaa1998@gmail.com>
2022-07-25 19:18:39 +0300

    add lesson 9

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -3,12 +3,13 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
 public class BaseTest {
-    WebDriver driver;
+   public WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
@@ -16,6 +17,12 @@ public class BaseTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+    }
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 }
